3e749ce8f8dc008812f0242304b35d9a18d344dc
João Victor Arbegaus <joao.v.arbegaus@gmail.com>
2024-11-10 18:50:25 -0300

    Atualiza Busca em Profundidade Iterativa (codigo refeito)

--- src/main/java/com/ViagemTrem/iaapi/algorithm/AprofundamentoIterativo.java
+++ src/main/java/com/ViagemTrem/iaapi/algorithm/AprofundamentoIterativo.java
@@ -5,12 +5,10 @@ import com.ViagemTrem.iaapi.model.MapaEuropa;
 import com.ViagemTrem.iaapi.model.ResultadoBusca;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 
 public class AprofundamentoIterativo implements AlgoritmoBusca {
 
@@ -18,93 +16,111 @@ public class AprofundamentoIterativo implements AlgoritmoBusca {
     int resets = 0;
 
     @Override
-    public ResultadoBusca buscar(Capital origemParam, Capital destinoParam) {
-
-        
+    public ResultadoBusca buscar(Capital origem, Capital destino) {
+        long startTime = System.nanoTime();
 
         // Mapa de capitais
         MapaEuropa mapa = new MapaEuropa();
-        
 
+        int nosGerados = 1; // Contador de nós gerados (a origem é o primeiro nó)
+        int nosExpandidos = 0; // Contador de nós expandidos
+        Boolean encontrado = false;
         // Variáveis para o resultado
         List<String> rota = new ArrayList<>();
         double distanciaTotal = 0.0;
-        int nosGerados = 0; // Contador de nós gerados
-        int nosExpandidos = 0; // Contador de nós expandidos
-        Capital destino = mapa.getCapital(destinoParam.getNome());
-        Capital origem = mapa.getCapital(origemParam.getNome());
-        
 
-        // Aprofundamento Iterativo, vai aumentado o limite a cada tentiva
-        for (int limite = 0; limite < 20; limite++) {
+        for (int profundidadeLimite = 0; encontrado == false; profundidadeLimite++) {
+
+            // Substitua a fila por uma pilha para realizar a busca em profundidade
+            Stack<Capital> pilha = new Stack<>();
+
+            // Set para armazenar capitais já visitadas
             Set<Capital> visitados = new HashSet<>();
-            resets++;
-            ResultadoBusca result = buscarComLimite(origem, destino, limite, visitados, nosGerados, nosExpandidos);
 
-            if (result != null) {
-                long endTime = System.nanoTime();
-                long tempoExecucao = endTime - startTime;
-                return result;
+            // Inicialize a pilha com a capital de origem
+            pilha.push(mapa.getCapital(origem.getNome()));
+
+            origem.setPai(null); // O nó inicial não tem pai
+            visitados.add(origem);
+
+            nosGerados = 1;
+            nosExpandidos = 0;
+
+            // Busca em profundidade (DFS) usando uma pilha
+            while (!pilha.isEmpty() && encontrado == false) {
+                Capital atual = pilha.pop(); // Remova o elemento do topo da pilha
+                nosExpandidos++;
+
+
+                // Se a profundidade do nó atual for maior que o limite, não expanda seus
+                // vizinhos
+                int profundidadeAtual = obterProfundidade(atual);
+
+
+                if (profundidadeAtual < profundidadeLimite) {
+                    for (Map.Entry<Capital, Integer> vizinho : atual.getVizinhos().entrySet()) {
+
+                        Capital vizinhoCapital = vizinho.getKey();
+
+                        if (!visitados.contains(vizinhoCapital)) {
+
+                            vizinhoCapital.setPai(atual); // Definimos o pai do vizinho para reconstruir a rota depois
+
+                            pilha.push(vizinhoCapital); // Adiciona o vizinho ao topo da pilha
+                            visitados.add(vizinhoCapital);
+                            nosGerados++;
+
+                            // Se o destino foi encontrado, reconstruímos o caminho e encerramos a busca
+                            if (vizinhoCapital.equals(destino)) {
+                                encontrado = true;
+                                Capital passo = vizinhoCapital;
+
+                                while (passo != null) {
+                                    rota.add(0, passo.getNome()); // Adiciona o nome da capital ao início da rota
+
+                                    if (passo.getPai() != null) {
+                                        distanciaTotal += passo.getPai().distanciaTremPara(passo); // Soma a distância
+                                                                                                   // entre
+                                                                                                   // o
+                                                                                                   // nó e seu
+                                                                                                   // pai
+                                    }
+
+                                    passo = passo.getPai(); // Vai para o pai para continuar reconstruindo
+                                }
+
+                                long endTime = System.nanoTime();
+                                long tempoExecucao = endTime - startTime;
+
+                                return new ResultadoBusca(rota, distanciaTotal, nosGerados, nosExpandidos,
+                                        tempoExecucao);
+
+                            }
+
+                        }
+                    }
+                }
+
+
             }
+
         }
-        return null; 
+        long endTime = System.nanoTime();
+        long tempoExecucao = endTime - startTime;
+
+        return new ResultadoBusca(rota, distanciaTotal, nosGerados, nosExpandidos,
+                tempoExecucao);
 
     }
 
-    private ResultadoBusca buscarComLimite(Capital atual, Capital destino, int limite, Set<Capital> visitados,
-            int nosGerados, int nosExpandidos) {
-        // Verifica se a profundidade limite foi atingida
-        if (limite == 0) {
-            return null;
+    // Método para calcular a profundidade de um nó
+    private int obterProfundidade(Capital capital) {
+        int profundidade = 0;
+        while (capital.getPai() != null) {
+            profundidade++;
+            capital = capital.getPai();
         }
-
-        // Marca o nó como visitado e expande
-        visitados.add(atual); 
-        nosExpandidos++;
-        
-
-        // Se encontrou o destino, inicia a reconstrução do caminho
-        if (atual.equals(destino)) {
-            List<String> rota = new ArrayList<>();
-            double distanciaTotal = 0.0;
-            Capital passo = destino;
-
-            while (passo != null) {
-                rota.add(0, passo.getNome()); // Adiciona o nome da capital ao início da rota
-
-                if (passo.getPai() != null) {
-                    distanciaTotal += passo.getPai().distanciaTremPara(passo); // Soma a distância entre o nó e seu pai
-                }
-
-                passo = passo.getPai(); // Vai para o pai para continuar reconstruindo
-            }
-            System.out.println("Resets: " + resets);
-            long endTime = System.nanoTime();
-            long tempoExecucao = endTime - startTime;
-            return new ResultadoBusca(rota, distanciaTotal, nosGerados, nosExpandidos, tempoExecucao); // Tempo de execução será
-                                                                                           // atualizado mais tarde
-        }
-
-        // Expande para cada vizinho, se ainda não foi visitado
-        for (Map.Entry<Capital, Integer> vizinho : atual.getVizinhos().entrySet()) {
-            Capital vizinhoCapital = vizinho.getKey();
-
-            if (!visitados.contains(vizinhoCapital)) {
-                vizinhoCapital.setPai(atual); // Define o pai para reconstruir o caminho depois
-                nosGerados++;
-
-                ResultadoBusca result = buscarComLimite(vizinhoCapital, destino, limite - 1, visitados, nosGerados,
-                        nosExpandidos); // reduz o limite, se n encontrar nada ele vai entrar no if(limite == 0)
-                if (result != null) {
-                    return result; // Encerra a busca se encontrar o destino
-                }
-
-                // Remove o nó do conjunto de visitados para permitir outras iterações no
-                // aprofundamento iterativo
-                visitados.remove(vizinhoCapital);
-            }
-        }
-        return null; // Retorna null se não encontrou o destino nesta profundidade
+        return profundidade;
     }
 
 }
